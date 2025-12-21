9052b87b574952878350c0b86b16552875a2cd09
aasmitha01 <tommandruaasmitha@gmail.com>
2025-12-21 19:14:31 +0530

    update theme styling and app structure

--- Mock-1/Code/src/App.jsx
+++ Mock-1/Code/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { PostsProvider } from "./context/PostsContext";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
+import PostsList from "./components/PostsList";
+import ThemeToggle from "./components/ThemeToggle";
+function AppContext(){
+  const {theme}=useTheme();
+  return( 
+    <div className={theme}>
+    <ThemeToggle/>
+    <PostsList/>
+    </div>
+  );
 }
-
-export default App
+function App(){
+  return(
+    <ThemeProvider>
+      <PostsProvider>
+        <AppContext/>
+      </PostsProvider>
+    </ThemeProvider>
+  );
+}
+export default App;
